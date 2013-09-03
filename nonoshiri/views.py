49802c5f50143fb3b9f6430f5f472c5a0ec5ad27
ardinor <temnoyume@gmail.com>
2013-09-03 11:38:24 +1000

    Basic twitter searching done

--- nonoshiri/views.py
+++ nonoshiri/views.py
@@ -1,7 +1,21 @@
 from flask import render_template
+from twython import Twython
 
 from nonoshiri import app
+from secret import CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_SECRET
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/q/<query>')
+def query(query):
+    twitter = Twython(CONSUMER_KEY, CONSUMER_SECRET,
+                      ACCESS_TOKEN, ACCESS_SECRET)
+    results = twitter.search(q=query)
+    statuses = results['statuses']
+    #results['search_metadata'].keys()
+    #['since_id_str', 'max_id_str', 'count', 'completed_in',
+    # 'refresh_url', 'query', 'max_id', 'since_id', 'next_results']
+    return render_template('index.html',
+                           statuses=statuses)
