3941444adc0e7b8f5652886c1a1b1e8a6b55b05a
ardinor <temnoyume@gmail.com>
2013-09-03 12:28:41 +1000

    Route tidy up

--- nonoshiri/views.py
+++ nonoshiri/views.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, jsonify
 from twython import Twython
 
 from nonoshiri import app
@@ -8,7 +8,7 @@ from secret import CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_SECRET
 def index():
     return render_template('index.html')
 
-@app.route('/q/<query>')
+@app.route('/api/q/<query>')
 def query(query):
     twitter = Twython(CONSUMER_KEY, CONSUMER_SECRET,
                       ACCESS_TOKEN, ACCESS_SECRET)
@@ -19,3 +19,4 @@ def query(query):
     # 'refresh_url', 'query', 'max_id', 'since_id', 'next_results']
     return render_template('index.html',
                            statuses=statuses)
+    #return jsonify(statuses)
